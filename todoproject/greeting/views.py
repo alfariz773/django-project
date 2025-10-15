f17165a4b5c254d2fb42998e683eefb917b161c5
alfariz773 <alfariza338@gmail.com>
2025-10-15 21:01:19 +0530

    printing hello world

--- todoproject/greeting/views.py
+++ todoproject/greeting/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+def greeting(request):
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
 
 # Create your views here.
--- todoproject/todoproject/urls.py
+++ todoproject/todoproject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from greeting import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.greeting),
+    path('about/',views.about),
 ]

48c599e64fc4bdcb0a396240cce5f20b7afb41fa
alfariz773 <alfariza338@gmail.com>
2025-10-16 10:37:40 +0530

    home work

---
+++ mysite/hello/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.gallery, name='gallery'),
+    path('contact/', views.contact, name='contact'),
+]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hello import views  # Import your view from the hello app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.gallery, name='gallery'),  # Root URL shows gallery
 ]
